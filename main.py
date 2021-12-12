5efaeb2944d0f52149be701c719121c30d0404fe
ShiderS <irbis26813@gmail.com>
2021-12-12 12:11:40 +0500

    Доработка определения темы.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import re, pymorphy2, csv
+
 # from search import *
 
 morph = pymorphy2.MorphAnalyzer()
@@ -12,18 +13,18 @@ themes_id = open('themes id.txt', encoding='utf-8').read().splitlines()
 file = []
 
 # Словарь id
-id_dict = {0: 'Общественный транспорт',
-           1: 'Социальная помощь',
-           2: 'Образовательные учреждения',
-           3: 'Спорт',
-           4: 'Организация отдыха и оздоровления детей',
-           5: 'Уборка снега',
-           6: 'Реклама и граффити',
-           7: 'Уличное освещение',
-           8: 'Мусор',
-           9: 'Дороги',
-           10: 'Дворы',
-           11: 'Парки'
+id_dict = {1: 'Общественный транспорт',
+           2: 'Социальная помощь',
+           3: 'Образовательные учреждения',
+           4: 'Спорт',
+           5: 'Организация отдыха и оздоровления детей',
+           6: 'Уборка снега',
+           7: 'Реклама и граффити',
+           8: 'Уличное освещение',
+           9: 'Мусор',
+           10: 'Дороги',
+           11: 'Дворы',
+           12: 'Парки'
            }
 
 # Словарь тем
@@ -43,13 +44,18 @@ for i in range(len(themes)):
 
 # Словарь ключевых слов
 dict_main_words = {}
-for i in range(12):
-    dict_main_words[i] = data[i].split()
+for i in range(1, 13):
+    dict_main_words[i] = data[i - 1].split()
 
 dict_main_words_themes = {}
 for i in range(len(data_themes)):
-    dict_main_words_themes[i] = data_themes[i].split()
-
+    if data_themes[i].isdigit():
+        a = int(data_themes[i])
+    else:
+        if a in dict_main_words_themes:
+            dict_main_words_themes[a].append(data_themes[i])
+        else:
+            dict_main_words_themes[a] = [data_themes[i]]
 # Открываем csv файл
 text = {}
 with open('primer.csv', encoding="utf8") as csvfile:
@@ -65,14 +71,17 @@ def search(text, main_word, id_tem, theme, file):
     text = re.split('\W', text)
     for i in range(len(text)):
         if text[i] != '' and len(text[i]) != 1:
-            for j in range(12):
+            for j in range(1, 13):
                 if morph.parse(text[i])[0].normal_form.lower() in dict_main_words[j]:
                     main_word = id_dict[j]
                     id_tem = j
-                    for p in range(len(dict_main_words_themes)):
-                        if morph.parse(text[i])[0].normal_form.lower() in dict_main_words_themes[p]:
-                            theme = themes_id[p]
-                        break
+    if id_tem != '' and main_word != '':
+        for i in range(len(text)):
+            for p in dict_main_words_themes[id_tem]:
+                if morph.parse(text[i])[0].normal_form.lower() in p and \
+                        len(morph.parse(text[i])[0].normal_form.lower()) > 1:
+                    dict_main_words_themes[id_tem].index(p)
+                    theme = dict_themes[id_tem][dict_main_words_themes[id_tem].index(p)]
     file.append({'cat_id': id_tem,
                  'cat_name': main_word,
                  'theme_name': theme,
